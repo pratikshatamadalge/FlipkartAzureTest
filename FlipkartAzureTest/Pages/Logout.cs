6f48b008c5bbb5b6104773f93778ab7239dcb03b
pratikshatamadalge <pratikshatamadalge21@gmail.com>
2020-10-09 10:05:33 +0530

    Refactored:Logout web element

--- FlipkartAzureTest/Pages/Logout.cs
+++ FlipkartAzureTest/Pages/Logout.cs
@@ -20,9 +20,9 @@
         [FindsBy(How = How.XPath, Using = "//body/div[@id='container']/div/div/div/div/div[3]/div[1]/div[1]/div[1]")]
         IWebElement MyAccountDropdown { get; set; }
 
-        [FindsBy(How = How.XPath, Using = "//li[10]//a[1]")]
-        IWebElement logout { get; set; }
-
+        [FindsBy(How = How.XPath, Using = "//body//div[@id='container']//div//div//div//div//div//li[10]//a[1]")]
+        IWebElement logoutBtn { get; set; }
+        
         public void LogoutPage()
         {
             Thread.Sleep(3000);
@@ -30,7 +30,7 @@
             Thread.Sleep(4000);
             MyAccountDropdown.Click();
             Thread.Sleep(5000);
-            logout.Click();
+            logoutBtn.Click();
             Thread.Sleep(5000);
         }
     }
